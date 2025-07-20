4eddfccd1ed5267c3fe23f081fd8e8276830731c
Lenovo <nagaboinapragathi@gmail.com>
2025-07-20 12:06:23 +0530

    new add

--- src/ex_01_Java_Basic/Lab001_Helloworld.java
+++ src/ex_01_Java_Basic/Lab001_Helloworld.java
@@ -1,8 +1,9 @@
+
 package ex_01_Java_Basic;
 
 public class Lab001_Helloworld {
-    public static void main (string[] args) {
-        System.out.println("Hello, I am running Java program")
+    public static void main (String[] args) {
+        System.out.println("Hello, I am running Java program");
     }
 }
 
---
+++ src/ex_01_Java_Basic/Lab003_operators.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab003_operators {
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 30;
+        boolean c = a > b;
+        System.out.println(c);
+
+        int age_abc = 20;
+        int age_def = 10;
+        boolean result = age_abc >= age_def;
+        System.out.println(result);
+
+    }
+}
